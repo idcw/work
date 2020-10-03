b38fc12b2444397b0a7fa204f95a4ab0d56499b9
iain <iain.idc@gmail.com>
2020-10-03 20:26:07 +0100

    jsx switch

---
+++ js/app.js
@@ -0,0 +1,143 @@
+
+class Header2 extends React.Component {
+  reload = () =>  {
+    window.location.reload(true)
+  };
+
+  initDB = () => {
+    //DB.init()
+  };
+
+  deleteDB = () => {
+    //DB.remove()
+  };
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    	  <a className="navbar-brand" href="#">Leave Manager</a>
+    	  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    	    <span className="navbar-toggler-icon"></span>
+    	  </button>
+
+    	  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    	    <ul className="navbar-nav mr-auto">
+    	      <li className="nav-item active">
+    	        <a className="nav-link" href="index.html">Personal Info Management <span className="sr-only">(current)</span></a>
+    	      </li>
+    	      <li className="nav-item">
+    	        <a className="nav-link" href="dashboard.html">Dashboard</a>
+    	      </li>
+
+    	    </ul>
+    			<ul className="navbar-nav navbar-right">
+    				<li className="nav-item dropdown">
+    					<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    						Settings
+    					</a>
+    					<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+    						<a className="dropdown-item" href="#" onClick={this.reload}>Reload</a>
+    						<a className="dropdown-item" href="#" onClick={this.initDB}>Init DB</a>
+    						<div className="dropdown-divider"></div>
+    						<a className="dropdown-item" href="#" onClick={this.deleteDB}>Delete DB</a>
+    					</div>
+    				</li>
+    			</ul>
+    	  </div>
+    	</nav>
+          );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { page: "list" };
+  }
+
+  reload = () =>  {
+    window.location.reload(true)
+  };
+
+  initDB = () => {
+    //DB.init()
+  };
+
+  deleteDB = () => {
+    //DB.remove()
+  };
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({
+      // page: !this.state.page
+    });
+  }
+
+  render() {
+    let status;
+    switch (this.state.page) {
+      case "list":
+        content = <EmployeeList></EmployeeList>;
+        break;
+      case false:
+        content = <p>OFF</p>;
+        break;
+      default:
+        content = null;
+        break;
+    }
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      	  <a className="navbar-brand" href="#">Leave Manager</a>
+      	  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      	    <span className="navbar-toggler-icon"></span>
+      	  </button>
+
+      	  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      	    <ul className="navbar-nav mr-auto">
+      	      <li className="nav-item active">
+      	        <a className="nav-link" href="index.html">Personal Info Management <span className="sr-only">(current)</span></a>
+      	      </li>
+      	      <li className="nav-item">
+      	        <a className="nav-link" href="dashboard.html">Dashboard</a>
+      	      </li>
+
+      	    </ul>
+      			<ul className="navbar-nav navbar-right">
+      				<li className="nav-item dropdown">
+      					<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      						Settings
+      					</a>
+      					<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+      						<a className="dropdown-item" href="#" onClick={this.reload}>Reload</a>
+      						<a className="dropdown-item" href="#" onClick={this.initDB}>Init DB</a>
+      						<div className="dropdown-divider"></div>
+      						<a className="dropdown-item" href="#" onClick={this.deleteDB}>Delete DB</a>
+      					</div>
+      				</li>
+      			</ul>
+      	  </div>
+      	</nav>
+        <button onClick={this.handleClick.bind(this)}>Toggle</button>
+        {content}
+      </div>
+    );
+  }
+
+}
+
+class EmployeeList extends React.Component {
+
+  render() {
+    return (
+      <div>List</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App></App>,
+  document.getElementById('root')
+);
