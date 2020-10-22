8a2f008d012a7c09a8cec1d780c02225cfee7ab5
idcw <iain.idc@gmail.com>
2020-10-22 21:59:21 +0100

    .

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
+/*
 // parse request params
 var q1 = $.url().param('q1');
 $('input[name="q1"]').val(q1);
 var q2 = $.url().param('q2');
 $('input[name="q2"]').val(q2);
+*/
+var q1 = null;
+var q2 = null;
 
 // read data from database
 var emps;
